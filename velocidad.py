0ba27d9b8a4305a1467c3ca9254e78ee8ffda8be
santiagocaroz <santoscaro98@gmail.com>
2021-01-13 12:33:42 -0500

    Deteccion Rising edge velocidad

--- velocidad.py
+++ velocidad.py
@@ -16,24 +16,27 @@ VEL=[]
 radio=1 #metros
 numero_huecos=6
 angulo=360/numero_huecos
-
+i=0
 while True:
-#    pulso=GPIO.input(pin_velocidad)
+    GPIO.wait_for_edge(pin_velocidad,GPIO.RISING)
+    i+=1
+    print(i)
+    pulso=GPIO.input(pin_velocidad)
     
-    pulso=int(input("p:"))# CAMBIAR POR 
-    if (pulso==0):
-        TIME.append(time.time()-time_start)
-        if (len(TIME)>=10):
-            TIME.pop(0)
-    
-        if(len(TIME)>=2):
-            for i in range(len(TIME)-1):
-                dt=TIME[i+1]-TIME[i]
-                VEL.append(angulo/dt)
-                if (len(VEL)>=10):
-                    VEL.pop(0)
-        
-            vel=sum(VEL)/len(VEL)*radio
-            print(vel)
+#     pulso=int(input("p:"))# CAMBIAR POR 
+#     if (pulso==0):
+#         TIME.append(time.time()-time_start)
+#         if (len(TIME)>=10):
+#             TIME.pop(0)
+#     
+#         if(len(TIME)>=2):
+#             for i in range(len(TIME)-1):
+#                 dt=TIME[i+1]-TIME[i]
+#                 VEL.append(angulo/dt)
+#                 if (len(VEL)>=10):
+#                     VEL.pop(0)
+#         
+#             vel=sum(VEL)/len(VEL)*radio
+#             print(vel)
             
    
