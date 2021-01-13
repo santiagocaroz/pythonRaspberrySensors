f523624f6119c8ef8cfd7dcd65f1cd0c67d3295c
santiagocaroz <santoscaro98@gmail.com>
2021-01-13 12:39:11 -0500

    Calculo de velocidad

--- velocidad.py
+++ velocidad.py
@@ -24,19 +24,18 @@ while True:
     pulso=GPIO.input(pin_velocidad)
     
 #     pulso=int(input("p:"))# CAMBIAR POR 
-#     if (pulso==0):
-#         TIME.append(time.time()-time_start)
-#         if (len(TIME)>=10):
-#             TIME.pop(0)
-#     
-#         if(len(TIME)>=2):
-#             for i in range(len(TIME)-1):
-#                 dt=TIME[i+1]-TIME[i]
-#                 VEL.append(angulo/dt)
-#                 if (len(VEL)>=10):
-#                     VEL.pop(0)
-#         
-#             vel=sum(VEL)/len(VEL)*radio
-#             print(vel)
+    TIME.append(time.time()-time_start)
+    if (len(TIME)>=10):
+        TIME.pop(0)
+
+    if(len(TIME)>=2):
+        for i in range(len(TIME)-1):
+            dt=TIME[i+1]-TIME[i]
+            VEL.append(angulo/dt)
+            if (len(VEL)>=10):
+                VEL.pop(0)
+    
+        vel=sum(VEL)/len(VEL)*radio
+        print(vel)
             
    
